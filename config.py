f91c6557865884ca5b6e19a02967bbc48ef28207
YanjunLi-CS <yanjun.li@ufl.edu>
2017-08-18 23:32:27 -0400

    config for modeling

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+config.py: 
+"""
+
+__author__ = "Yanjun Li"
+__license__ = "MIT"
+
+NUM_EPOCHS = 1
+MAX_LENGTH = 120
+BATCH_SIZE = 600
+LATENT_DIM = 292
+RANDOM_SEED = 1337
---
+++ molecules/model_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+model_1.py: 
+"""
+
+__author__ = "Yanjun Li"
+__license__ = "MIT"
