c613df5a620350ad5380ce28c82a45b7ec43cde8
YanjunLi-CS <yanjun.li@ufl.edu>
2017-08-18 23:33:15 -0400

    update

--- molecules/model_1.py
+++ molecules/model_1.py
@@ -2,8 +2,120 @@
 # -*- coding: utf-8 -*-
 
 """
-model_1.py: 
+model_1.py:
+Re-implement the VAE model for own purpose
 """
+
+from keras import backend as K
+from keras import objectives
+from keras.models import Model
+from keras.layers import Input, Dense, Lambda
+from keras.layers.core import Dense, Activation, Flatten, RepeatVector
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.recurrent import GRU
+from keras.layers.convolutional import Conv1D
+from config import *
 
 __author__ = "Yanjun Li"
 __license__ = "MIT"
+
+
+class MoleculeVAE(object):
+    def __init__(self):
+        self.encoder = None
+        self.decoder = None
+        self.autoencoder = None
+
+    def create(self, charset, max_length=MAX_LENGTH, latent_rep_size=LATENT_DIM, weights_file=None):
+
+        charset_length = len(charset)
+
+        x = Input(shape=(max_length, charset_length))
+        _, z = self._buildEncoder(x, latent_rep_size, max_length)
+        self.encoder = Model(x, z)
+        pass
+
+
+    def create(self,
+               charset,
+               max_length=MAX_LENGTH,
+               latent_rep_size=LATENT_DIM,
+               weights_file=None):
+
+        charset_length = len(charset)
+
+        x = Input(shape=(max_length, charset_length))
+        _, z = self._buildEncoder(x, latent_rep_size, max_length)
+        self.encoder = Model(x, z)
+
+        encoded_input = Input(shape=(latent_rep_size,))
+        self.decoder = Model(
+            encoded_input,
+            self._buildDecoder(
+                encoded_input,
+                latent_rep_size,
+                max_length,
+                charset_length
+            )
+        )
+
+        x1 = Input(shape=(max_length, charset_length))
+        vae_loss, z1 = self._buildEncoder(x1, latent_rep_size, max_length)
+        self.autoencoder = Model(
+            x1,
+            self._buildDecoder(
+                z1,
+                latent_rep_size,
+                max_length,
+                charset_length
+            )
+        )
+
+        if weights_file:
+            self.autoencoder.load_weights(weights_file)
+            self.encoder.load_weights(weights_file, by_name=True)
+            self.decoder.load_weights(weights_file, by_name=True)
+
+        self.autoencoder.compile(optimizer='Adam',
+                                 loss=vae_loss,
+                                 metrics=['accuracy'])
+
+    def _buildEncoder(self, x, latent_rep_size, max_length, epsilon_std=0.01):
+        h = Conv1D(filters=9, kernel_size=9, padding='valid', activation='relu', name='conv_1')(x)
+        h = Conv1D(filters=9, kernel_size=9, padding='valid', activation='relu', name='conv_2')(h)
+        h = Conv1D(filters=10, kernel_size=11, padding='valid', activation='relu', name='conv_3')(h)
+        h = Flatten(name='flatten_1')(h)
+        h = Dense(435, activation='relu', name='dense_1')(h)
+
+        def sampling(args):
+            z_mean_, z_log_var_ = args
+            batch_size = K.shape(z_mean_)[0]
+            epsilon = K.random_normal(shape=(batch_size, latent_rep_size), mean=0., stddev=epsilon_std)
+            return z_mean_ + K.exp(z_log_var_ / 2) * epsilon
+
+        z_mean = Dense(latent_rep_size, name='z_mean', activation='linear')(h)
+        z_log_var = Dense(latent_rep_size, name='z_log_var', activation='linear')(h)
+
+        def vae_loss(x, x_decoded_mean):
+            x = K.flatten(x)
+            x_decoded_mean = K.flatten(x_decoded_mean)
+            xent_loss = max_length * objectives.binary_crossentropy(x, x_decoded_mean)
+            kl_loss = - 0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+            return xent_loss + kl_loss
+
+        o = Lambda(sampling, output_shape=(latent_rep_size,), name='lambda')([z_mean, z_log_var])
+        return vae_loss, o
+
+    def _buildDecoder(self, z, latent_rep_size, max_length, charset_length):
+        h = Dense(latent_rep_size, name='latent_input', activation='relu')(z)
+        h = RepeatVector(max_length, name='repeat_vector')(h)
+        h = GRU(501, return_sequences=True, name='gru_1')(h)
+        h = GRU(501, return_sequences=True, name='gru_2')(h)
+        h = GRU(501, return_sequences=True, name='gru_3')(h)
+        return TimeDistributed(Dense(charset_length, activation='softmax'), name='decoded_mean')(h)
+
+    def save(self, filename):
+        self.autoencoder.save_weights(filename)
+
+    def load(self, charset,  weights_file, max_length, latent_rep_size=292):
+        self.create(charset, max_length=max_length, latent_rep_size=latent_rep_size, weights_file=weights_file)
